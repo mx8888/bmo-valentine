1caf1c2ade0bd1bcd36fcc665033e36b03bdb65f
mx8888 <88298798+mx8888@users.noreply.github.com>
2025-02-03 01:44:02 +0800

    Add files via upload

---
+++ BMO/script.js
@@ -0,0 +1,44 @@
+function moveButton() {
+    var noBtn = document.getElementById('no-btn');
+    var maxWidth = window.innerWidth - noBtn.offsetWidth;
+    var maxHeight = window.innerHeight - noBtn.offsetHeight;
+    var x = Math.random() * maxWidth;
+    var y = Math.random() * maxHeight;
+
+    x = Math.max(0, Math.min(x, maxWidth));
+    y = Math.max(0, Math.min(y, maxHeight));
+
+    noBtn.style.left = `${x}px`;
+    noBtn.style.top = `${y}px`;
+}
+document.getElementById("yes-btn").addEventListener("click", function () {
+    alert("YIIEE IKAW AH HAHAHAHAH");
+  
+    // Trigger heart particles
+    hearts();
+  });
+  
+  function hearts() {
+    const heartContainer = document.getElementById('heart-container');
+  
+    heartContainer.innerHTML = '';
+  
+
+    for (let i = 0; i < 69; i++) { 
+      const heart = document.createElement('span');
+      const size = Math.random() * 60 + 30; 
+      const xPos = Math.random() * 100; 
+      const yPos = Math.random() * 80 + 10; 
+      const delay = Math.random() * 5; 
+  
+ 
+      heart.classList.add('particle');
+      heart.style.width = `${size}px`;
+      heart.style.height = `${size}px`;
+      heart.style.left = `${xPos}%`;
+      heart.style.top = `${yPos}%`;
+      heart.style.animationDelay = `${delay}s`;
+  
+      heartContainer.appendChild(heart);
+  }
+  }  
